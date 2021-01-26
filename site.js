57b13cbce086ec6a71c51674b14b792dcc5a24d9
Huldson Richard <huldson-_-@hotmail.com>
2021-01-26 19:19:53 -0300

    site de troco

--- site.js
+++ site.js
@@ -1,13 +1,14 @@
-let button = document.querySelector("#add_nome")// pega oque tiver no arquiovo do button
+let button = document.querySelector("#add_nome")// pega oque tiver no arquivo do button
 button.addEventListener("click",function(evento){
-    let nome = document.querySelector("#nome")//são as tudo que tem nesse documento as id tem que ser igual a do html
-    let ultimonome = document.querySelector("#ultimonome")
-    let idade = document.querySelector("#idade")
-    let nometeste = nome.value // value da o valor que tem no documento 
-    let ultimonometeste = ultimonome.value
-    let idadeteste = idade.value
-    console.log(nometeste,ultimonometeste,idadeteste)
-    let x = document.querySelector("#log")
+   // let nome = document.querySelector("#nome")//são as tudo que tem nesse documento as id tem que ser igual a do html
+   // let ultimonome = document.querySelector("#ultimonome")
+   // let idade = document.querySelector("#idade")
+    //let nometeste = nome.value // value da o valor que tem no documento 
+    //let ultimonometeste = ultimonome.value
+    //let idadeteste = idade.value
+   // console.log(nometeste,ultimonometeste,idadeteste)
+    //let x = document.querySelector("#log")
+    console.log("casa")
    // let lista = document.querySelector("#list")
    // lista.innerHTML = "<br>"+ "<h4>"+input.value+"</h4>" + lista.innerHTML 
   x.innerHTML = "<br>"+"<h1>"+nometeste+"  "+ultimonometeste+idadeteste
---
+++ sitenotas.js
@@ -0,0 +1,81 @@
+
+let botao = document.querySelector("#add_nota")
+
+
+
+botao.addEventListener("click", function(){
+    let cedula = document.querySelector("#reais")
+    let cedul = cedula.value
+    let nota = +cedul
+   
+     let print = document.querySelector("#qw")
+    console.log( typeof nota)
+    console.log(nota[2])
+   
+    if(typeof nota === 'number'){
+    let nota1=0,nota5=0,nota10=0,nota20=0,nota50=0,nota100=0
+   console.log(nota)
+     while(nota>=100){
+        nota100++
+        nota=nota-100
+   
+    }
+    if(nota100!=0){
+  //  print.innerHTML = nota100 +' notas de R$100' + print.innerHTM
+    }
+
+    while(nota>=50){
+   
+       nota50++
+       nota=nota-50
+    }
+    if(nota50!=0){
+       // print.innerHTML =  nota50+' notas de R$50' + print.innerHTM
+    }
+    while(nota>=20){
+   
+        nota20++
+        nota=nota-20 
+    }  
+    if(nota20!=0){
+       // print.innerHTML = nota20+' notas de R$20' + print.innerHTM
+    }  
+    while(nota>=10){
+        nota10++
+        nota=nota-10
+     
+    }
+    if(nota10!=0){
+      //  print.innerHTML = nota10+' notas de R$10' +print.innerHTM
+    }
+    while(nota>=05){
+        nota5++
+        nota=nota-05
+    } 
+    if(nota5!=0){
+       // print.innerHTML = nota5+' notas de R$5' +print.innerHTM
+    }
+   while(nota>=01){
+       nota1++       
+       nota=nota-01 
+    }
+       if(nota1!=0){
+       // print.innerHTML= nota1+' moedas de R$1' +print.innerHTM
+    }
+    print.innerHTML = nota100 +' notas de R$100 '+'<br>' + nota50+' notas de R$50 '+'<br>' + nota20+' notas de R$20'+'<br>' + nota10+' notas de R$10'+'<br>' +  nota1+' moedas de R$1'
+}
+    else{
+
+        print.innerHTML= "digite um valor valido" 
+    }
+    
+    
+
+    
+}
+    
+    
+    
+    
+    )
+   
